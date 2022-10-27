2299388dd58b4af2e49c5c730edb92ec39dc32a8
Alexandre Hertogs <_ahertogs.az@sodexo.com>
2022-10-27 15:37:55 +0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <Counter />
+      </header>
+    </div>
+  );
+}
+
+export default App;
