174fded1628dcb2d91d28ef78ad8a6b5e2618ef1
Alexandre Hertogs <_ahertogs.az@sodexo.com>
2022-10-27 16:16:53 +0400

    remove css

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./App";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
